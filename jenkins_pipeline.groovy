810c64c76a1e0c00b9bab76f8d5592cf75352802
justynauzik11 <justyna.guzik11@gmail.com>
2023-03-24 11:48:33 -0400

    pipeline script update

--- jenkins_pipeline.groovy
+++ jenkins_pipeline.groovy
@@ -3,17 +3,17 @@ pipeline {
     stages {
         stage('Build') { 
             steps {
-                // 
+                echo "building";
             }
         }
         stage('Test') { 
             steps {
-                // 
+                echo "test stage";
             }
         }
         stage('Deploy') { 
             steps {
-                // 
+                echo "deploy stage";
             }
         }
     }
