79b75f5ec51515a694e0fb0ae5903476c34dbd2e
justynauzik11 <justyna.guzik11@gmail.com>
2023-04-25 08:50:49 -0400

    groovy script added

---
+++ jenkins_pipeline.groovy
@@ -0,0 +1,30 @@
+timeout(time: 15, unit: 'MINUTES') {
+    node {
+        try {
+            stage('Checkout') {
+                cleanWs()
+                checkout(scm)
+            }
+            stage('Run') {
+                docker.build('python-docker', ".").inside {
+                    script {
+                        cmd = "echo python costam"
+                        sh(script: cmd)
+                    }
+                }
+            }
+        } catch (err) {
+            currentBuild.result = "FAILED"
+            print 'Build failed'
+            print err
+        }
+        finally {
+            stage("Post-build") {
+                script {
+                    println "Post-build anyway"
+                }
+            cleanWs()
+            }
+        }
+    }
+}
